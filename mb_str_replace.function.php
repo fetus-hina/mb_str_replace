b40cc2f3a543a4ba4e04ae15ae50f67b7d227b18
HiNa <hina@bouhime.com>
2011-06-14 17:40:22 +0900

    * 最近使っているコーディングスタイルに変更 * 単純な文字連結に sprintf() を使わない * ライセンスを 3 条項 BSD から 2 条項 BSD に変更

--- mb_str_replace.function.php
+++ mb_str_replace.function.php
@@ -1,79 +1,84 @@
 <?php
-	// THIS FILE WRITTEN IN UTF-8, Japanese.
-	
-	/**
-	 * マルチバイト対応 str_replace
-	 * 
-	 * @version		Release 2
-	 * @author		HiNa (hina@bouhime.com)
-	 * @copyright	Copyright (C) 2006-2007 by HiNa(hina@bouhime.com).
-	 */
-	
-	if(! function_exists('mb_str_replace')) {
-		/**
-		 * マルチバイト対応 str_replace
-		 * 
-		 * @param	mixed	$search		検索文字列
-		 * @param	mixed	$replace	置換文字列
-		 * @param	mixed	$subject	対象文字列
-		 * @param	string	$encoding	文字列のエンコーディング(省略: 内部エンコーディング)
-		 *
-		 * @return	mixed	subject 内の search を replace で置き換えた文字列
-		 *
-		 * @note	この関数は配列に対応(search, replace, subject)しています。
-		 */
-		function mb_str_replace($search, $replace, $subject, $encoding = 'auto') {
-			if(! is_array($search)) {
-				$search = array($search);
-			}
-			if(! is_array($replace)) {
-				$replace = array($replace);
-			}
-			if(strtolower($encoding) === 'auto') {
-				$encoding = mb_internal_encoding();
-			}
-			if(is_array($subject)) {
-				$result = array();
-				foreach($subject as $key => $val) {
-					$result[$key] = mb_str_replace($search, $replace, $val, $encoding);
-				}
-				return $result;
-			}
-			
-			$currentpos = 0;
-			while(true) {
-				$index = -1;
-				$minpos = -1;
-				foreach($search as $key => $find) {
-					if($find == '') {
-						continue;
-					}
-					$findpos = mb_strpos($subject, $find, $currentpos, $encoding);
-					if($findpos !== false) {
-						if($minpos < 0 || $findpos < $minpos) {
-							$minpos = $findpos;
-							$index = $key;
-						}
-					}
-				}
-				if($minpos < 0) {
-					break;
-				}
-				
-				$r = array_key_exists($index, $replace) ? $replace[$index] : '';
-				$subject = sprintf('%s%s%s',
-										mb_substr($subject, 0, $minpos, $encoding),
-										$r,
-										mb_substr(
-											$subject,
-											$minpos + mb_strlen($search[$index], $encoding),
-											mb_strlen($subject, $encoding),
-											$encoding
-										)
-									);
-				$currentpos = $minpos + mb_strlen($r, $encoding);
-			}
-			return $subject;
-		}
-	}
-?>
+/**
+ * マルチバイト対応 str_replace()
+ * 
+ * @version		Release 3
+ * @author      HiNa <hina@bouhime.com>
+ * @copyright   Copyright (C) 2006-2007,2011 by HiNa <hina@bouhime.com>. All rights reserved.
+ * @license     2-cause BSD License(Simplified BSD License)
+ * @link        http://fetus.k-hsu.net/document/programming/php/mb_str_replace.html
+ * @link        https://github.com/fetus-hina/mb_str_replace
+ */
+if(!function_exists('mb_str_replace')) {
+    /**
+     * マルチバイト対応 str_replace()
+     * 
+     * @param   mixed   $search     検索文字列（またはその配列）
+     * @param   mixed   $replace    置換文字列（またはその配列）
+     * @param   mixed   $subject    対象文字列（またはその配列）
+     * @param   string  $encoding   文字列のエンコーディング(省略: 内部エンコーディング)
+     *
+     * @return  mixed   $subject 内の $search を $replace で置き換えた文字列
+     *
+     * @note    この関数の $search, $replace, $subject は配列に対応していますが、PHP 標準の str_replace() と挙動が異なります。
+     */
+    function mb_str_replace($search, $replace, $subject, $encoding = 'auto') {
+        if(!is_array($search)) {
+            $search = array($search);
+        }
+        if(!is_array($replace)) {
+            $replace = array($replace);
+        }
+        if(strtolower($encoding) === 'auto') {
+            $encoding = mb_internal_encoding();
+        }
+
+        // $subject が複数ならば各要素に繰り返し適用する
+        if(is_array($subject) || $subject instanceof Traversable) {
+            $result = array();
+            foreach($subject as $key => $val) {
+                $result[$key] = mb_str_replace($search, $replace, $val, $encoding);
+            }
+            return $result;
+        }
+
+        $currentpos = 0;    // 現在の検索開始位置
+        while(true) {
+            // $currentpos 以降で $search のいずれかが現れる位置を検索する
+            $index = -1;    // 見つけた文字列（最も前にあるもの）の $search の index
+            $minpos = -1;   // 見つけた文字列（最も前にあるもの）の位置
+            foreach($search as $key => $find) {
+                if($find == '') {
+                    continue;
+                }
+                $findpos = mb_strpos($subject, $find, $currentpos, $encoding);
+                if($findpos !== false) {
+                    if($minpos < 0 || $findpos < $minpos) {
+                        $minpos = $findpos;
+                        $index = $key;
+                    }
+                }
+            }
+
+            // $search のいずれも見つからなければ終了
+            if($minpos < 0) {
+                break;
+            }
+
+            // 置換実行
+            $r = array_key_exists($index, $replace) ? $replace[$index] : '';
+            $subject =
+                mb_substr($subject, 0, $minpos, $encoding) .    // 置換開始位置より前
+                $r .                                            // 置換後文字列
+                mb_substr(                                      // 置換終了位置より後ろ
+                    $subject,
+                    $minpos + mb_strlen($search[$index], $encoding),
+                    mb_strlen($subject, $encoding),
+                    $encoding);
+
+            // 「現在位置」を $r の直後に設定
+            $currentpos = $minpos + mb_strlen($r, $encoding);
+        }
+        return $subject;
+    }
+}
